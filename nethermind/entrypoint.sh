347b198011cda17151301828beb0feced9fad74c
dappnodedev <diego@dappnode.io>
2024-06-27 17:17:11 +0200

    Move P2P port to compose

--- nethermind/entrypoint.sh
+++ nethermind/entrypoint.sh
@@ -1,41 +1,47 @@
 #!/bin/sh
 
-set_jwt_path() {
-  CLIENT=$1
-  echo "Using $CLIENT JWT"
-  JWT_PATH="/security/$CLIENT/jwtsecret.hex"
-}
-
 set_network_specific_config() {
-  CONSENSUS_DNP=$1
-  P2P_PORT=$2
-
   echo "[INFO - entrypoint] Initializing $NETWORK specific config for client"
 
+  set_consensus_dnp
+
   # If consensus client is prysm-prater.dnp.dappnode.eth --> CLIENT=prysm
-  CLIENT=$(echo "$CONSENSUS_DNP" | cut -d'.' -f1 | cut -d'-' -f1)
+  consensus_client=$(echo "$CONSENSUS_DNP" | cut -d'.' -f1 | cut -d'-' -f1)
 
-  set_jwt_path "$CLIENT"
-
+  set_jwt_path "$consensus_client"
 }
 
-case "$NETWORK" in
-"gnosis") set_network_specific_config "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_GNOSIS" 31404 ;;
-"holesky") set_network_specific_config "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_HOLESKY" 31804 ;;
-"mainnet") set_network_specific_config "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_MAINNET" 30404 ;;
-*)
-  echo "Unsupported network: $NETWORK"
-  exit 1
-  ;;
-esac
+set_consensus_dnp() {
+  uppercase_network=$(echo "$NETWORK" | tr '[:lower:]' '[:upper:]')
+  consensus_dnp_var="_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_${uppercase_network}"
+  eval "CONSENSUS_DNP=\${$consensus_dnp_var}"
+}
 
-# Print the jwt to the dappmanager
-JWT=$(cat "${JWT_PATH}")
-curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}"
+set_jwt_path() {
+  consensus_client=$1
+  echo "[INFO - entrypoint] Using $consensus_client JWT"
+  JWT_PATH="/security/$consensus_client/jwtsecret.hex"
 
-exec /nethermind/nethermind \
-  --config "${NETWORK}" \
-  --JsonRpc.JwtSecretFile="${JWT_PATH}" \
-  --Network.DiscoveryPort="${P2P_PORT}" \
-  --Network.P2PPort="${P2P_PORT}" \
-  "${EXTRA_OPTS}"
+  if [ ! -f "${JWT_PATH}" ]; then
+    echo "[ERROR - entrypoint] JWT not found at ${JWT_PATH}"
+    exit 1
+  fi
+}
+
+post_jwt_to_dappmanager() {
+  echo "[INFO - entrypoint] Posting JWT to DAppManager"
+  JWT=$(cat "${JWT_PATH}")
+
+  curl -X POST "http://my.dappnode/data-send?key=jwt&data=${JWT}" || {
+    echo "[ERROR - entrypoint] JWT could not be posted to package info"
+  }
+}
+
+run_client() {
+  # shellcheck disable=SC2086
+  exec /nethermind/nethermind \
+    --config "${NETWORK}" \
+    --JsonRpc.JwtSecretFile="${JWT_PATH}" \
+    --Network.DiscoveryPort="${P2P_PORT}" \
+    --Network.P2PPort="${P2P_PORT}" ${EXTRA_OPTS}
+}
