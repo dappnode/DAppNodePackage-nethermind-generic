def2f9702f99a4ca5a30b42cb2acc7708598d7bb
dappnodedev <diego@dappnode.io>
2024-07-22 17:54:46 +0200

    Use latest staker scripts version

--- nethermind/entrypoint.sh
+++ nethermind/entrypoint.sh
@@ -5,15 +5,15 @@ SUPPORTED_NETWORKS="holesky mainnet gnosis"
 # shellcheck disable=SC1091
 . /etc/profile
 
-run_client() {
-  # shellcheck disable=SC2086
-  exec /nethermind/nethermind \
-    --config "${NETWORK}" \
-    --JsonRpc.JwtSecretFile="${JWT_PATH}" \
-    --Network.DiscoveryPort="${P2P_PORT}" \
-    --Network.P2PPort="${P2P_PORT}" ${EXTRA_OPTS}
-}
+JWT_PATH=$(get_jwt_path "${NETWORK}" "${SUPPORTED_NETWORKS}" "${SECURITY_PATH}")
 
-set_execution_config_by_network "${SUPPORTED_NETWORKS}"
-post_jwt_to_dappmanager
-run_client
+post_jwt_to_dappmanager "${JWT_PATH}"
+
+echo "[INFO - entrypoint] Starting Nethermind client for network: ${NETWORK}"
+
+# shellcheck disable=SC2086
+exec /nethermind/nethermind \
+  --config "${NETWORK}" \
+  --JsonRpc.JwtSecretFile="${JWT_PATH}" \
+  --Network.DiscoveryPort="${P2P_PORT}" \
+  --Network.P2PPort="${P2P_PORT}" ${EXTRA_OPTS}
